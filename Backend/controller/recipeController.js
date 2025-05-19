a520242bf9c00f644c0a485ca21dd1470efd0af4
manjotsingh0850 <manjot0850.be21@chitkara.edu.in>
2025-05-19 23:59:20 +0530

    modified recipeController, recipe file inside Route folder.

--- Backend/controller/recipeController.js
+++ Backend/controller/recipeController.js
@@ -2,5 +2,21 @@ const getRecipes=(req,res)=>{
     res.json({message:"hello i am in controller"})
 }
 
-module.exports={getRecipes}
+const getRecipe=(req,res)=>{
+    res.json({message:"getting the recipe by ID"})
+}
+
+const addRecipe=(req, res)=>{
+    res.json({message: "(adding)posting the recipe"})
+}
+
+const editRecipe=(req, res)=>{
+    res.json({message: "edit Recipe by ID"})
+}
+
+const deleteRecipe=(req, res)=>{
+    res.json({message:"delete recipe by ID"})
+}
+
+module.exports={getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe}
 
--- Backend/routes/recipe.js
+++ Backend/routes/recipe.js
@@ -1,8 +1,16 @@
 const express=require("express")
-const { getRecipes } = require("../controller/recipeController")
+const { getRecipes, addRecipe, editRecipe, deleteRecipe } = require("../controller/recipeController")
+const {getRecipe} = require("../controller/recipeController")
+
 const router=express.Router()
 
-router.get("/",getRecipes)
+router.get("/",getRecipes) // router for getting all the recipes.
+router.get("/:id", getRecipe) // get recipe using id
+router.post("/", addRecipe) // to add recipe
+router.put("/:id", editRecipe) // Edit Recipe
+router.delete("/:id", deleteRecipe) // to delete recipe by id
+
+
 // router.get("/",(req,res)=>{
 //     res.json({message:"hello router! Hi I am in recipe.js"})
 // })
